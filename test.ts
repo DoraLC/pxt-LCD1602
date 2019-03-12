614eb1480d05052feee907ff40ebebc1106863b5
DoraLC <34760377+DoraLC@users.noreply.github.com>
2019-03-12 18:03:20 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,8 @@
 let item = 0
-I2C_LCD1602.LcdInit(0)
-I2C_LCD1602.ShowString("Hello", 0, 0)
+LCD1602.LcdInit(0)
+LCD1602.ShowString("Hello", 0, 0)
 basic.forever(() => {
     item += 1
-    I2C_LCD1602.ShowNumber(item, 0, 1)
+    LCD1602.ShowNumber(item, 0, 1)
     basic.pause(1000)
 })
